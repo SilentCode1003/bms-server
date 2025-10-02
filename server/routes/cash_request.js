88d9643dfed6eaeab7456338e7e2716f7906f057
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2025-10-02 10:24:13 +0800

    [FIX] cash request

--- server/routes/cash_request.js
+++ server/routes/cash_request.js
@@ -179,7 +179,7 @@ router.get("/getexisting_liquidation", async (req, res) => {
 
                         let select_cash_request_rejected_no_liquidation_sql = SelectStatement(
                                 `SELECT
-                                cr_id AS id
+                                '' AS id
                                 FROM cash_request cr
                                 LEFT JOIN liquidation l ON cr.cr_reference_id = l.l_cr_reference_id
                                 WHERE cr_employee_id = ? and isnull(l.l_status)`,
